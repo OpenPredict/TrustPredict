5bb71529b88fb3b02f0fc42a1b8f5451c011c2a9
riordant <riordant@tcd.ie>
2020-09-03 14:36:09 +0700

    Fixes; divide contracts into separate files

--- contracts/StableCoin.sol
+++ contracts/StableCoin.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.6.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract StableCoin is ERC20, ERC20Burnable {
+contract StableCoin is ERC20 {
 
   constructor () public ERC20("Stablecoin Token", "SCT") {
     _mint(msg.sender, 10000000 * (10 ** uint256(decimals())));
