f8f6fb5a90a8db0020ba35681b08cc725622b1a3
DAS REPTIL <paddy@reptile.haus>
2020-10-08 17:01:02 +0200

    events pages added

--- frontend/src/app/components/app-header/app-header.component.ts
+++ frontend/src/app/components/app-header/app-header.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthQuery } from '@app/services/auth-service/auth.service.query';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss'],
 })
-export class AppHeaderComponent implements OnInit {
+export class AppHeaderComponent {
 
-  constructor(private navCtrl: NavController) { }
-
-  ngOnInit() {}
-
+  loggedIn$: Observable<boolean> = this.authQuery.select( user => !!user.wallet )
+  
+  constructor(
+    private navCtrl: NavController,
+    public authQuery: AuthQuery ) { }
 
   openOptions() {
     this.navCtrl.navigateForward('landing');
