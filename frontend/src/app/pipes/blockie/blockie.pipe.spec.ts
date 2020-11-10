6190ab2ffdb758aa42be6446f82364bc80633580
DAS REPTIL <paddy@reptile.haus>
2020-11-10 16:48:28 +0100

    blockie added

---
+++ frontend/src/app/pipes/blockie/blockie.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { BlockiePipe } from './blockie.pipe';
+
+describe('BlockiePipe', () => {
+  it('create an instance', () => {
+    const pipe = new BlockiePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
