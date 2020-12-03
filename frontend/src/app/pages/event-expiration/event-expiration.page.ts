28655bc3a6d735a78b27d0a594d58e2854a1a3ab
ljupcho <rljupcho@gmail.com>
2020-12-04 00:29:11 +0100

    datePicker implemented

--- frontend/src/app/pages/event-expiration/event-expiration.page.ts
+++ frontend/src/app/pages/event-expiration/event-expiration.page.ts
@@ -1,67 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { OptionService } from '@services/option-service/option.service';
-import { NavController } from '@ionic/angular';
-import { Observable } from 'rxjs';
-import { OptionQuery } from '@services/option-service/option.service.query';
-import { BaseForm } from '@app/helpers/BaseForm';
-import { FormBuilder, Validators } from '@angular/forms';
-import { OptionsStore } from '@app/services/option-service/option.service.store';
-import * as moment from 'moment';
+import { Component, OnInit } from "@angular/core";
+import { OptionService } from "@services/option-service/option.service";
+import { NavController } from "@ionic/angular";
+import { Observable } from "rxjs";
+import { OptionQuery } from "@services/option-service/option.service.query";
+import { BaseForm } from "@app/helpers/BaseForm";
+import { FormBuilder, Validators } from "@angular/forms";
+import { OptionsStore } from "@app/services/option-service/option.service.store";
+import * as moment from "moment";
+import { ThemePalette } from "@angular/material/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-event-expiration',
-  templateUrl: './event-expiration.page.html',
-  styleUrls: ['./event-expiration.page.scss'],
+  selector: "app-event-expiration",
+  templateUrl: "./event-expiration.page.html",
+  styleUrls: ["./event-expiration.page.scss"]
 })
-export class EventExpirationPage  extends BaseForm implements OnInit {
-
+export class EventExpirationPage extends BaseForm implements OnInit {
   loading$: Observable<boolean>;
 
   min: string;
 
-  modalHeader = 'Event Settlement';
+  modalHeader = "Event Settlement";
   modalTxt = `
     <p>
       Choose a date at which you would like the event to settle. It is at this time that your event will
       'finalize': the event will receive the current price of the asset at the time you choose here.
     </p>`;
 
+  public date: moment.Moment;
+  public minDate: moment.Moment;
+  public maxDate: moment.Moment;
+  public color: ThemePalette = "primary";
 
   constructor(
     private fb: FormBuilder,
     private optService: OptionService,
     private optQry: OptionQuery,
     private optStr: OptionsStore,
-    public navCtrl: NavController ) {
-      super();
-      this.form = this.fb.group({
-        expiration_date: [this.min, Validators.compose([Validators.required])],
-      });
-    }
+    public navCtrl: NavController
+  ) {
+    super();
+    this.form = this.fb.group({
+      expiration_date: [this.min, Validators.compose([Validators.required])]
+    });
+  }
 
-    ngOnInit() {
-    this.min = moment().add(2, 'days').toISOString();
-    }
+  ngOnInit() {
+    this.min = moment().add(2, "days").toISOString();
+  }
 
-    continue() {
-      this.setSubmitted();
-      if (!this.form.valid) {
-        return;
-      }
-      try {
-        const expiry = this.form.controls['expiration_date'].value;
-        const a = moment();
-        const b = moment(expiry);
-        const seconds =  a.diff(b, 'seconds');
-        this.optStr.upsert(1, { expiration_date: Number( Math.abs(seconds) ) } );
-        this.navCtrl.navigateForward([`/launch-option`]);
-      } catch (error) {
-        console.log(`Error: ${error}`);
-       }
+  continue() {
+    this.setSubmitted();
+    if (!this.form.valid) {
+      return;
     }
-
-    goBack() {
-      this.navCtrl.back();
+    try {
+      const expiry = this.form.controls["expiration_date"].value;
+      const a = moment();
+      const b = moment(expiry);
+      const seconds = a.diff(b, "seconds");
+      this.optStr.upsert(1, { expiration_date: Number(Math.abs(seconds)) });
+      this.navCtrl.navigateForward([`/launch-option`]);
+    } catch (error) {
+      console.log(`Error: ${error}`);
     }
+  }
 
+  goBack() {
+    this.navCtrl.back();
+  }
 }
