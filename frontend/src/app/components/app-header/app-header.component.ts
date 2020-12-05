410a44722bcc4c9dcc0acf2cb072b550206c1cb1
ljupcho <rljupcho@gmail.com>
2020-12-05 13:15:00 +0100

    logo image redirects to landing page

--- frontend/src/app/components/app-header/app-header.component.ts
+++ frontend/src/app/components/app-header/app-header.component.ts
@@ -4,6 +4,7 @@ import { ModalController, NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { InformationModalComponent } from "@components/information-modal/information-modal.component";
 import { WalletOptionsModalComponent } from '@components/wallet-options-modal/wallet-options-modal.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-app-header',
@@ -12,66 +13,67 @@ import { WalletOptionsModalComponent } from '@components/wallet-options-modal/wa
 })
 export class AppHeaderComponent implements OnInit {
 
-  loggedIn$: Observable<boolean> = this.authQuery.select( user => !!user.wallet );
+  loggedIn$: Observable<boolean> = this.authQuery.select(user => !!user.wallet);
   address: string;
   @Input() modal_header: string;
   @Input() modal_txt: string;
-  chainName: any;  
-  
+  chainName: any;
+
   constructor(
     private navCtrl: NavController,
     private cd: ChangeDetectorRef,
     private zone: NgZone,
     public modalCtrl: ModalController,
-    public authQuery: AuthQuery  ) { }
+    public authQuery: AuthQuery,
+    private router: Router) { }
 
-    ngOnInit() {
-      this.authQuery.select( user => user ).subscribe( res => {
-        this.zone.run( async () => {
-          if(res && res.wallet) {
-            console.log( res.chainName )
-            this.address = res.wallet
-            this.chainName = res.chainName
-            this.cd.detectChanges();               
-          }
-        });          
+  ngOnInit() {
+    this.authQuery.select(user => user).subscribe(res => {
+      this.zone.run(async () => {
+        if (res && res.wallet) {
+          console.log(res.chainName)
+          this.address = res.wallet
+          this.chainName = res.chainName
+          this.cd.detectChanges();
+        }
+      });
     });
-    }
+  }
 
   openOptions() {
     this.navCtrl.navigateForward('landing');
   }
-  
+
   openEvents() {
     this.navCtrl.navigateForward('my-events');
   }
-  
+
   openEventsFinished() {
     this.navCtrl.navigateForward('event-completed');
   }
-  
+
   async information() {
     try {
       const modalOpts = {
         component: InformationModalComponent,
         componentProps: {
           "modal_header": this.modal_header,
-          "modal_txt": this.modal_txt,          
+          "modal_txt": this.modal_txt,
         },
         cssClass: 'opt-modal',
       };
       const modal: HTMLIonModalElement = await this.modalCtrl.create(modalOpts);
       await modal.present();
       const selection = await modal.onDidDismiss();
-      if ( selection.data ) {
+      if (selection.data) {
         console.log(selection.data);
       }
     } catch (error) {
-       console.log(`modal present error ${error}`);
-       throw error;
+      console.log(`modal present error ${error}`);
+      throw error;
     }
-  }  
-  
+  }
+
   async options() {
     try {
       const modalOpts = {
@@ -83,13 +85,17 @@ export class AppHeaderComponent implements OnInit {
       const modal: HTMLIonModalElement = await this.modalCtrl.create(modalOpts);
       await modal.present();
       const selection = await modal.onDidDismiss();
-      if ( selection.data ) {
+      if (selection.data) {
         console.log(selection.data);
       }
     } catch (error) {
-       console.log(`modal present error ${error}`);
-       throw error;
+      console.log(`modal present error ${error}`);
+      throw error;
     }
+  }
+
+  navigateToLandingPage() {
+    this.router.navigateByUrl('/landing');
   }
 
 }
