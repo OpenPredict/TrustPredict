0467c243eaf5fdb993f2ffb6dd45577a48ebc581
riordant <riordant@tcd.ie>
2020-12-17 12:37:53 +0700

    Add getPrice starter code

--- frontend/src/app/pages/event-condition/event-condition.page.ts
+++ frontend/src/app/pages/event-condition/event-condition.page.ts
@@ -9,6 +9,8 @@ import { OptionsStore } from '@app/services/option-service/option.service.store'
 import { CustomValidators } from '@app/helpers/CustomValidators';
 import { AppHeaderComponent } from "@components/app-header/app-header.component";
 const axios = require('axios');
+const parser = require('node-html-parser');
+const ethers = require('ethers');
 
 @Component({
   selector: 'app-event-condition',
@@ -20,6 +22,7 @@ export class EventConditionPage extends BaseForm implements OnInit {
   loading$: Observable<boolean>;
   formattedAmount: any;
   dollarMask = BaseForm.dollarMask;
+  usdPrice: any;
   @ViewChild("header") header: AppHeaderComponent;
 
   modalHeader = 'Choosing Event Conditions'
@@ -44,14 +47,23 @@ export class EventConditionPage extends BaseForm implements OnInit {
     this.form.get('wager').setValidators([CustomValidators.numberRange(0.01, Number.MAX_VALUE)]);
   }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.getPrice(); 
+  }
 
-  async getPrice(address) {
+  async getPrice() {
+    const currentOptions = this.optQry.getAll();
+    const pairContract = currentOptions[0].pair_contract;
+    console.log('pairContract: ' + pairContract);
     const response = await axios({
-          baseURL: `https://kovan.etherscan.io/readContract?m=normal&a=${address}&v=0x44D01B3F44E8351E60296bdF57B27CF2659B0200#`,
+      baseURL: `https://kovan.etherscan.io/readContract?m=normal&a=${pairContract}&v=0x44D01B3F44E8351E60296bdF57B27CF2659B0200#`,
       method: 'post',
-      });
-      return response;
+    });
+    const document = parser.parse(response.data);
+    const price = document.querySelectorAll('#readCollapse8')[0].text.replace(' int256','');
+    console.log('price: ' + price);
+    this.usdPrice = parseFloat(ethers.utils.formatUnits(price.toString(), 8)).toFixed(2);
+    console.log('this.usdPrice: ' + this.usdPrice);
   };
 
   continue() {
