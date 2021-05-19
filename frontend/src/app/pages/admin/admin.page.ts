8e29e2939f8211595edda54bf86026807e640aa3
Ljupcho <rljupcho@gmail.com>
2021-05-19 22:04:26 +0200

    admin style changes

--- frontend/src/app/pages/admin/admin.page.ts
+++ frontend/src/app/pages/admin/admin.page.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.page.html',
-  styleUrls: ['./admin.page.scss'],
+    selector: 'app-admin',
+    templateUrl: './admin.page.html',
+    styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
 
-  constructor() {
-  }
+    availableOptions = [{ name: 'Dao Maker' }, { name: 'test 1' }, { name: 'test 2' }];
+    constructor(private fb: FormBuilder) {
+        
+    }
 
-  ngOnInit() {
-    
-  }
+    ngOnInit() {
 
-  ngOnDestroy() { }
+    }
 
-  
+    ngOnDestroy() { }
+
+
 }
